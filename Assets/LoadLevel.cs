b61eabd9a0dd4d869a337d7e8d3bddad97a09b39
Stangil <stangil44@gmail.com>
2019-01-10 14:03:52 -0500

    Added Spacecraft and particle effects

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] float reloadTime = 2f;
+
+    void Start()
+    {    
+        Invoke("LoadFirstLevel", reloadTime);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            LoadFirstLevel();
+        }
+    }
+    void LoadFirstLevel()
+    { 
+       SceneManager.LoadScene(1);
+    }
+    
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float movementSpeed = 20;//using same speed for horiz and vert
+    [Tooltip("In m")] [SerializeField] float xLimit = 4f;
+    [Tooltip("In m")] [SerializeField] float yLimitMax = 2f;
+    [Tooltip("In m")] [SerializeField] float yLimitMin = -5f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 4f;
+    [SerializeField] float controlYawFactor = 30f;
+    [SerializeField] float controlRollFactor = -40f;
+    private float xThrow, yThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        //TODO Take away pitch and yaw from contoler when movement is maxed out
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yawDueToControlThrow = xThrow * controlYawFactor;      
+        float yaw = yawDueToPosition + yawDueToControlThrow;
+        
+        float roll = controlRollFactor * xThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * movementSpeed * Time.deltaTime;
+        float yOffset = yThrow * movementSpeed * Time.deltaTime;
+
+        float newXPos = transform.localPosition.x + xOffset;//takes current x pos and adds xOffset
+        float newYPos = transform.localPosition.y + yOffset;//takes current y pos and adds yOffset
+
+        float clampedXPos = Mathf.Clamp(newXPos, -xLimit, xLimit);//Limiting left right movement
+        float clampedYPos = Mathf.Clamp(newYPos, yLimitMin, yLimitMax);//Limiting left right movement
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
